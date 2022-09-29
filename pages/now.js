862beeaa1fcbf3cb03c3abbcdebef3de21792d8f
Einar <einargudnig@gmail.com>
2022-09-29 15:04:47 +0000

    new collection

--- pages/now.js
+++ pages/now.js
@@ -13,14 +13,16 @@ export default function Now() {
       <div>
         <div className="my-2">
           <h3>Where am I and what am I doing?</h3>
-          <div className="mt-3 mb-4 text-sm">Last updated: 4 September 2022</div>
+          <div className="mt-3 mb-4 text-sm">Last updated: 28 September 2022</div>
         </div>
         {/* Misc */}
         <div className="flex justify-between">
           <div className="text-sm p-1 mt-2 mb-10 border border-gray-600 dark:border-gray-200 rounded-md w-1/4">
-            <span className="font-semibold">Location:</span> <span>Reykjavík</span>
+            <span className="font-semibold">Location:</span> <span>The Coffe Colletive,</span>{' '}
             <br />
-            <span className="font-semibold">Weather:</span> <span>11°C, fog</span>
+            <span>Copenhagen</span>
+            <br />
+            <span className="font-semibold">Weather:</span> <span>10°C, cloudy</span>
           </div>
 
           <div className="text-sm p-1 mt-2 mb-10 border border-gray-600 dark:border-gray-200 rounded-md w-2/5">
@@ -62,7 +64,7 @@ export default function Now() {
           <br />
           <p>
             I recently started a project to update a part of our data to match our new data model.
-            It's not the most fun projerct to do, but it's important for us to finish this project.
+            It's not the most fun project to do, but it's important for us to finish this project.
           </p>
           <p>It will help us a lot in regard to other projects we have.</p>
           <br />
@@ -111,15 +113,29 @@ export default function Now() {
           </p>
           <br />
           <p>
-            I started to play around with{' '}
+            I went to Oktoberfest in Munich with some friend, I though it would be fun to make a
+            drink counter and a bingo!
+          </p>
+          <br />
+          {''}
+          <p>
+            You can see the live webpage{' '}
             <Link
               href={'https://remix.run/'}
               className="special-underline hover:dark:text-gray-800 dark:text-gray-100 no-underline"
             >
-              remix.
+              here.
             </Link>{' '}
-            The hype is real. I've been wanting to dive more into it and get to know it better. I'll
-            write a blog post about it soon ans share my thoughts.
+          </p>
+          <p>
+            If you want to read more details about this project you can do that{' '}
+            <Link
+              href={'https://remix.run/'}
+              className="special-underline hover:dark:text-gray-800 dark:text-gray-100 no-underline"
+            >
+              here
+            </Link>{' '}
+            on my blog.
           </p>
         </div>
         <div className="mt-3 text-sm">
