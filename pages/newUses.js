56131d01433a4ceb1d0187885b1cbcc67ab42c71
Einar <einargudnig@gmail.com>
2022-10-02 21:59:49 +0000

    new ssh, who dis?

--- pages/newUses.js
+++ pages/newUses.js
@@ -2,5 +2,11 @@ import { PageSEO } from '@/components/SEO'
 import siteMetadata from '@/data/siteMetadata'
 
 export default function NewUses() {
-  return <p>Hi guys, this will be my new uses page. It will be cool!</p>
+  return (
+    <div>
+      <p>Hi guys, this will be my new uses page. It will be cool!</p>
+      <br />
+      <p>This is a test to see if I can push this easily to git after I updated my SSH.</p>
+    </div>
+  )
 }
