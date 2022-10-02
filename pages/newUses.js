aefacd522f586c838af451792cb980b1bfa2d8eb
Einar <einargudnig@gmail.com>
2022-10-02 21:55:53 +0000

    testing new github ssh

--- pages/newUses.js
+++ pages/newUses.js
@@ -2,5 +2,5 @@ import { PageSEO } from '@/components/SEO'
 import siteMetadata from '@/data/siteMetadata'
 
 export default function NewUses() {
-  return <p>look at me!</p>
+  return <p>Hi guys, this will be my new uses page. It will be cool!</p>
 }
